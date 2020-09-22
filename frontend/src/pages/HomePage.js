dee6b9e22ffd7fd4897a2196891b5088a72da55e
Nguyen Quang Tri <nguyenquangtri@Nguyens-MacBook-Air.local>
2020-09-22 11:11:00 +0700

    38. Building the Features Section

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/layout/Header';
 import SectionAbout from '../components/sections/section-about';
+import SectionFeatures from '../components/sections/section-features';
 
 function HomePage(props) {
     return (
         <div className="HomePage">
             <Header />
             <SectionAbout />
+            <SectionFeatures />
         </div>
     )
 }
