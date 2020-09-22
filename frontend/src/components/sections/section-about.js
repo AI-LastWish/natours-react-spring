e4cf8640633a5e16ed5f8f7acb580e22f2e7d36c
Nguyen Quang Tri <nguyenquangtri@Nguyens-MacBook-Air.local>
2020-09-22 09:33:38 +0700

    36. Building the About Section - Part 2

--- frontend/src/components/sections/section-about.js
+++ frontend/src/components/sections/section-about.js
@@ -4,17 +4,27 @@ import '../../scss/main.scss';
 const sectionAbout = props => {
     return (
         <section className="section-about">
-            <div className="u-center-text u-margin-bottom-lg">
+            <div className="u-center-text u-margin-bottom-big">
                 <h2 className="heading-secondary">
                     Exciting tours for adventurous people
                 </h2>
             </div>
 
-            <div class="row">
-                <div class="col-1-of-2">
-                    Text content
+            <div className="row">
+                <div className="col-1-of-2">
+                    <h3 className="heading-tertiary u-margin-bottom-small">You're going to fall in love with nature</h3>
+                    <p className="paragraph">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+                    </p>
+
+                    <h3 className="heading-tertiary u-margin-bottom-small">Live adventures like you never have before</h3>
+                    <p className="paragraph">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo quo dolore, inventore corrupti minus dicta!
+                    </p>
+
+                    <a href="#" className="btn-text">Learn more &rarr;</a>
                 </div>
-                <div class="col-1-of-2">
+                <div className="col-1-of-2">
                     Image composition
                 </div>
             </div>
