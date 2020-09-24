049d721c6047e8c171a88e6f452c25a579df804a
Nguyen Quang Tri <nguyenquangtri@Nguyens-MacBook-Air.local>
2020-09-24 20:31:05 +0700

    47. Building the Booking Section - Part 3

--- frontend/src/components/sections/section-book.js
+++ frontend/src/components/sections/section-book.js
@@ -23,6 +23,29 @@ const sectionBook = props => {
                                 <input type="email" className="form__input" placeholder="Email address" id="form__input--email" required />
                                 <label htmlFor="form__input--email" className="form__label">Email address</label>
                             </div>
+
+                            <div className="form__group">
+                                <div className="form__radio-group">
+                                    <input type="radio" className="form__radio-input" id="form__radio-input--small"
+                                        name="form__radio-input--size" />
+                                    <label htmlFor="form__radio-input--small" className="form__radio-label">
+                                        <span className="form__radio-button"></span>
+                                        Small tour group
+                                    </label>
+                                </div>
+                                <div className="form__radio-group">
+                                    <input type="radio" className="form__radio-input" id="form__radio-input--large"
+                                        name="form__radio-input--size" />
+                                    <label htmlFor="form__radio-input--large" className="form__radio-label">
+                                        <span className="form__radio-button"></span>
+                                        Large tour group
+                                    </label>
+                                </div>
+                            </div>
+
+                            <div className="form__group">
+                                <button className="btn btn--green">Next step &rarr;</button>
+                            </div>
                         </form>
                     </div>
                 </div>
