a78e4e1a62854fbe083b0555cd4440862f0bf8d2
Nguyen Quang Tri <nguyenquangtri@Nguyens-MacBook-Air.local>
2020-09-26 16:02:11 +0700

    52. Building a Pure CSS Popup - Part 1

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -7,6 +7,7 @@ import SectionStories from '../components/sections/section-stories';
 import SectionBook from '../components/sections/section-book';
 import Footer from '../components/layout/Footer';
 import Navigation from '../components/layout/Navigation';
+import Popup from '../components/popup/Popup';
 
 function HomePage(props) {
     return (
@@ -19,6 +20,7 @@ function HomePage(props) {
             <SectionStories />
             <SectionBook />
             <Footer />
+            <Popup />
         </div>
     )
 }
